31f75f1f9f049c3e000c3e3f08b792af6863d2ee
Viroon Yong <vyong@udel.edu>
2018-04-09 13:19:30 -0400

    Added UI stuff so that now you can custom query the API

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 // index.html's js
-var url = 'https://newsapi.org/v2/everything?' +
+/*
+This was the example url provided by newsapi.org
+*/
+var exampleurl = 'https://newsapi.org/v2/everything?' +
 					'q=Apple&' +
 					'from=2018-04-09&' +
 					'sortBy=popularity&' +
 					'apiKey=1473d5d8b9b54099940acc059d5cb4be';
 
-$.getJSON(url, function (data) {
+
+function submitAPIRequest(){
+	var url = 'https://newsapi.org/v2/everything?' +
+				'q=' + $("input[name='Query'").val() + "&" +
+				'from=2018-04-09&' + // Remove this line later
+				'sortBy=' + $('select[name="SortBy"] :selected').val() + "&" +
+				'apiKey=1473d5d8b9b54099940acc059d5cb4be';
+	$.getJSON(url, function (data) {
+		// Clear the content div
+		$("#Content").html("");
 		// data = the API's json response
 		console.log(data);
 		// Iterate through the articles
 		var articles = data.articles;
-		var htmlstring = "";
 		articles.forEach(function (element) {
-			htmlstring += "<p>";
+			var htmlstring = "";
 			htmlstring += "<div>Author: " + element.author + "</div>";
 			htmlstring += "<div>Title: " + element.title + "</div>";
-			htmlstring += "</p>";
+			htmlstring += "<div>URL: " + element.url + "</div>";
+			htmlstring += "<hr/>";
+			$("#Content").append(htmlstring);
 		});
-		$("#Content").html(htmlstring);
 	});
+}
