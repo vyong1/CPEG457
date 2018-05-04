14332895826b96855469a78d06b8c00d02b6a44b
vyong <vyong@udel.edu>
2018-05-04 12:29:11 -0400

    Initial commit for PHP-python jerry-rigging

---
+++ php/python/HtmlWriter.py
@@ -0,0 +1,26 @@
+class HtmlFile():
+    Name = 'pipe'
+    
+    def write(htmlText):
+        f = open(HtmlFile.Name, "w")
+        f.write(htmlText)
+        f.close()
+    
+    def append(htmlText):
+        f = open(HtmlFile.Name, "a")
+        f.write(htmlText)
+        f.close()
+    
+    def clear():
+        f = open(HtmlFile.Name, "w")
+        f.close()
+
+    def read():
+        f = open(HtmlFile.Name, "r")
+        return f.read()
+
+# Sample Usage:
+#
+# HtmlFile.write("Dank\n")
+# HtmlFile.append("Memes")
+#
