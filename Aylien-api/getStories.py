5df0b60b4cb2cc014505b4b5d22c206a9804d138
Frank Hulmes <fdhulmes@udel.edu>
2018-05-06 12:34:21 -0400

    Added functions for each task. Created a list of authors

--- Aylien-api/getStories.py
+++ Aylien-api/getStories.py
@@ -21,20 +21,16 @@ aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-Key'] = 'a92
 # create an instance of the API class
 api_instance = aylien_news_api.DefaultApi()
 
-url = "https://www.cnn.com/2018/04/24/politics/melania-trump-white-house/index.html"
-html = urllib.request.urlopen(url)
-soup = BeautifulSoup(html, 'html.parser')
-raw_text = soup.get_text()
-
 # This needs to be changed to match your NER directory
 st = StanfordNERTagger('C:/Users/Francis/Documents/Search and Data Mining/CPEG457/Aylien-api/NER/classifiers/english.all.3class.distsim.crf.ser.gz',
                          'C:/Users/Francis/Documents/Search and Data Mining/CPEG457/Aylien-api/NER/stanford-ner.jar',
                          encoding='utf-8')
 
-tokenized_text = word_tokenize(raw_text)
-classified_text = st.tag(tokenized_text)
+################################################
 
-print(classified_text)
+# Function Declarations
+
+################################################
 
 def getStories(author_name):
     # Takes in authors name, sets it as an option for the api
@@ -49,8 +45,47 @@ def getStories(author_name):
     except ApiException as e:
         print("Exception when calling DefaultApi->list_stories: %sn" % e)
         
+def createRawText(url):
+    # takes in the url and creates the raw HTML text
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    raw_text = soup.get_text()
+    return raw_text
+
+def createTags(raw_text):
+    tokenized_text = word_tokenize(raw_text)
+    classified_text = st.tag(tokenized_text)
+    return classified_text
+
+def createPossibleAuthor(tags):
+    author_list = []
+    for x in range(0, len(tags)):
+        if (tags[x][1] == 'PERSON'):
+            name = tags[x][0]
+            if (tags[x + 1][1] == 'PERSON'):
+                name += " " + tags[x + 1][0]
+                x += 1
+            author_list.append(name)                   
+        elif (tags[x][0] == 'by'):
+            author_list.append(tags[x][0])
+    return author_list
+            
+
+input_url = "https://www.cnn.com/2018/05/05/politics/trump-nra-speech-angers-french/index.html"
+
+tagged_text = createTags(createRawText(input_url))
+possible_authors = createPossibleAuthor(tagged_text)
+print(possible_authors)
+
+
+
+
+
+
+
         
-author = "Kevin Liptak"
+        
+author = "Adam Shaw"
 api_response = getStories(author)
 for element in api_response.stories:
         print(element.title)
