54fc699b36defc8f01c1f0ca3f4b0de07e163b33
vyong <vyong@udel.edu>
2018-05-06 21:19:50 -0400

    Added embedded twitter example, added bootstrap cdn

--- php/Index.php
+++ php/Index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     </head>
     <body>
         <form action="FormSubmit.php" method="post">
@@ -13,10 +15,11 @@
                 <label for="occupation">Enter an occupation:</label>
                 <input type="text" name="occupation">
             </div>
-            
+
             <div>
                 <input type="submit">
             </div>
         </form>
+        <a href="EmbeddedTwitter.php">Click here to see a twitter feed in action</a>
     </body>
 </html>
