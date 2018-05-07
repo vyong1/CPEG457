f4941e181b21f0464e45f8adf24a133a851614a0
vyong <vyong@udel.edu>
2018-05-06 21:29:56 -0400

    Added static (js, css) resources. Added bootstrap cdn.

--- WebApp/FormSubmit.php
+++ WebApp/FormSubmit.php
@@ -3,31 +3,35 @@
     <head>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <script src="static\index.js"></script>
+        <link rel="stylesheet" href="static\site.css" />
     </head>
     <body>
-        <?php
-        $cmd = 'py python/HtmlBuilder.py "' . $_POST['name'] . '"';
-        
-        // Clear the pipe
-        $pipefile = fopen("pipe", "w") or die("Unable to open pipe");
-        fclose($pipefile);
+        <div class="content">
+            <?php
+            $cmd = 'py python/HtmlBuilder.py "' . $_POST['name'] . '"';
+            
+            // Clear the pipe
+            $pipefile = fopen("pipe", "w") or die("Unable to open pipe");
+            fclose($pipefile);
 
-        // Write the $_POST data to the pipe
-        $pipefile = fopen("pipe", "a") or die("Unable to open pipe");
-        foreach($_POST as $key => $value)
-        {
-            fwrite($pipefile, $key .':'. $value . PHP_EOL);
-        }
-        fclose($pipefile);
-        
-        // Call appropriate python script to build up the file to the pipe
-        exec('py python/HtmlBuilder.py');
+            // Write the $_POST data to the pipe
+            $pipefile = fopen("pipe", "a") or die("Unable to open pipe");
+            foreach($_POST as $key => $value)
+            {
+                fwrite($pipefile, $key .':'. $value . PHP_EOL);
+            }
+            fclose($pipefile);
+            
+            // Call appropriate python script to build up the file to the pipe
+            exec('py python/HtmlBuilder.py');
 
-        // Read the pipe file
-        $pipefile = fopen("pipe", "r") or die("Unable to open pipe");
-        echo fread($pipefile, filesize("pipe"));
-        fclose($pipefile);
-        ?>
-        <a href="index.php">Click here to go back to index.php</a>
+            // Read the pipe file
+            $pipefile = fopen("pipe", "r") or die("Unable to open pipe");
+            echo fread($pipefile, filesize("pipe"));
+            fclose($pipefile);
+            ?>
+            <a href="index.php">Click here to go back to index.php</a>
+        </div>
     </body>
 </html>
--- WebApp/Index.php
+++ WebApp/Index.php
@@ -3,23 +3,27 @@
     <head>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <script src="static\index.js"></script>
+        <link rel="stylesheet" href="static\site.css" />
     </head>
     <body>
-        <form action="FormSubmit.php" method="post">
-            <div>
-                <label for="name">Enter a name:</label>
-                <input type="text" name="name">
-            </div>
+        <div class="content">
+            <form action="FormSubmit.php" method="post">
+                <div class="form-group">
+                    <label for="name_input">Name</label>
+                    <input type="text" class="form-control" id="name_input" name="name" placeholder="Your Name">
+                </div>
 
-            <div>
-                <label for="occupation">Enter an occupation:</label>
-                <input type="text" name="occupation">
-            </div>
+                 <div class="form-group">
+                    <label for="occupation_input">Occupation</label>
+                    <input type="text" class="form-control" id="occupation_input" name="occupation" placeholder="Your Name">
+                </div>
 
-            <div>
-                <input type="submit">
-            </div>
-        </form>
-        <a href="EmbeddedTwitter.php">Click here to see a twitter feed in action</a>
+                <div>
+                    <input type="submit">
+                </div>
+            </form>
+            <a href="EmbeddedTwitter.php">Click here to see a twitter feed in action</a>
+        </div>
     </body>
 </html>
