61f3b65c268eb27aa122b8a83415e1eb32984824
vyong <vyong@udel.edu>
2018-05-09 18:07:07 -0400

    Added NameStats.py

---
+++ WebApp/python/Aylien_api/NameStats.py
@@ -0,0 +1,12 @@
+class NameStats():
+    def __init__(self, TF, nameIsInTitle, nameIsInLink, preceededByKeyWord, firstOccurence);
+        self.TF = TF
+        self.nameIsInTitle = nameIsInTitle
+        self.nameIsInLink = nameIsInLink
+        self.preceededByKeyWord = preceededByKeyWord
+        self.firstOccurence = firstOccurence
+
+    def calculateProbability(self):
+        raise Exception("Not Implemented Exception")
+    
+
