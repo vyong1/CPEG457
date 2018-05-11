7b368fcb67ee7a232f67bac91cc5c1c9c05452a7
Frank Hulmes <fdhulmes@yahoo.com>
2018-05-11 11:29:43 -0400

    Possible solution for byline

--- WebApp/python/test.py
+++ WebApp/python/test.py
@@ -12,6 +12,8 @@ from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 # beatiful soup import
 from bs4 import BeautifulSoup
+# Regular Expression import
+import re
 
 
 # Establish the path to java
@@ -31,7 +33,7 @@ aylien_news_api.configuration.api_key['X-AYLIEN-NewsAPI-Application-Key'] = '655
  #                        'C:/Users/Frank/My Documents/Search and Data Mining/CPEG457/WebApp/python/Aylien_api/NER/stanford-ner.jar',
   #                       encoding='utf-8')
 
-input_url = "http://www.foxnews.com/politics/2018/05/10/trumps-summit-with-kim-jong-un-set-for-june-12-in-singapore-president-says.html"
+input_url = "https://www.cnn.com/2018/05/11/politics/donald-trump-foreign-policy/index.html"
 
 # tagged_text = Aylien_api.getStories.createTags(Aylien_api.getStories.createRawText(input_url))
 # possible_authors = Aylien_api.getStories.createPossibleAuthor(tagged_text)
@@ -41,12 +43,45 @@ html = urllib.request.urlopen(input_url)
 soup = BeautifulSoup(html, 'html.parser')
 raw_text = soup.get_text()
 #print(soup)
-byline_index = str(soup).find('author-byline')
-byline = ''
-for i in range(0, 50):
-    byline += str(soup)[byline_index + i]
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
 
-print(byline)
+def getByline(inputSoup):
+    byline_index = str(inputSoup).find('by')
+    htmlLine = ''
+    byline = ''
+    flag = False
+
+    for i in range(-20, 20):
+        htmlLine += str(inputSoup)[byline_index + i]
+
+    for j in htmlLine:
+        if(j == '"'):
+            flag = not flag
+        elif(flag):
+            byline += j
+    print(byline)
+    output = inputSoup.find(class_ = byline)
+    return output
+
+def searchAuthor(inputSoup, possibleAuthor):
+    author_indexes = find_all(str(inputSoup), possibleAuthor)
+    allLines = []
+    currentLine = '' 
+    line_count = 0
+    for element in author_indexes:
+        for i in range(-30, 30):
+            currentLine += str(inputSoup)[element + i]
+        allLines.append(currentLine)
+        currentLine = ''
+    return allLines
+
+print(searchAuthor(soup, 'Stephen Collinson'))
 author_dict = {}
 
 # for author in possible_authors:
